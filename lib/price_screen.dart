4dd9417f971f7e7ffcb90a2bd4e2b6d2763c47ca
Mohamed ElMorsy <mohammed.abdulaziz@gmail.com>
2024-03-12 12:27:35 +0000

    Implement currency selector widget per platform

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +11,10 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+  String selectedValue = currenciesList[0];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +36,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ? $selectedValue',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,10 +51,63 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: buildCurrencySelector(),
           ),
         ],
       ),
     );
+  }
+
+  CupertinoPicker buildCupertinoPicker() {
+    List<Text> widgets = buildItemWidgets();
+    return CupertinoPicker(
+      children: widgets,
+      itemExtent: 32,
+      onSelectedItemChanged: (selectedIndex) {
+        setState(() {
+          String? selectedData = widgets[selectedIndex].data;
+          if(selectedData != null) {
+            selectedValue = selectedData;
+          }
+        });
+
+      },
+    );
+  }
+
+  DropdownButton<String> buildDropdownButton() {
+    return DropdownButton<String>(
+            value: selectedValue,
+            items: buildItems(),
+            onChanged: (newValue){
+              setState(() {
+                if(newValue != null) {
+                  selectedValue = newValue;
+                }
+              });
+            },
+          );
+  }
+
+  List<DropdownMenuItem<String>> buildItems() {
+    return buildItemWidgets()
+        .map((e) => DropdownMenuItem(
+              child: e,
+              value: e.data,
+            ))
+        .toList();
+  }
+
+  List<Text> buildItemWidgets() {
+    return currenciesList
+        .map((e) => Text(e),)
+        .toList();
+  }
+
+  buildCurrencySelector() {
+    if(Platform.isIOS) {
+      return buildCupertinoPicker();
+    }
+    return buildDropdownButton();
   }
 }
